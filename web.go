28bce2cb41ea05ee5038edcd716c4dd5258231c5
avhadpooja <36248454+avhadpooja@users.noreply.github.com>
2018-03-04 17:49:12 -0500

    Update web.go

--- web.go
+++ web.go
@@ -22,10 +22,27 @@ func main() {
 	//fmt.Fprintln(res, "hello, heroku")
 }*/
 
-func hello(res http.ResponseWriter, req *http.Request) {
-	err :=r.ParseForm()
-	if err != nil{}
-	domains := r.PostFormFile("myfile")
-	fmt.Fprintf(w, "List is %s", myfile)
-	
-	//fmt.Fprintln(res, "hello, heroku")
+func hello(w http.ResponseWriter, r *http.Request) {
+	var Buf bytes.Buffer
+    // in your case file would be fileupload
+    file, header, err := r.FormFile("myfile")
+    if err != nil {
+        panic(err)
+    }
+    defer myfile.Close()
+    name := strings.Split(header.Filename, ".")
+    fmt.Printf("File name %s\n", name[0])
+    // Copy the file data to my buffer
+    io.Copy(&Buf, file)
+    // do something with the contents...
+    // I normally have a struct defined and unmarshal into a struct, but this will
+    // work as an example
+    contents := Buf.String()
+    fmt.Println(contents)
+    // I reset the buffer in case I want to use it again
+    // reduces memory allocations in more intense projects
+    Buf.Reset()
+    // do something else
+    // etc write header
+    return
+}

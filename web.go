fb6c735d0d5792d5e7797cc5d88b8af6cdc5773c
avhadpooja <36248454+avhadpooja@users.noreply.github.com>
2018-03-04 18:07:51 -0500

    Update web.go

--- web.go
+++ web.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
 	"net/http"
 	"os"
 	"html/template"
 	"io"
+	"strconv"
+	"time"
 )
 
 func main() {
@@ -24,26 +27,25 @@ func main() {
 }*/
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	var Buf bytes.Buffer
-    // in your case file would be fileupload
-    file, header, err := r.FormFile("myfile")
-    if err != nil {
-        panic(err)
-    }
-    defer myfile.Close()
-    name := strings.Split(header.Filename, ".")
-    fmt.Printf("File name %s\n", name[0])
-    // Copy the file data to my buffer
-    io.Copy(&Buf, file)
-    // do something with the contents...
-    // I normally have a struct defined and unmarshal into a struct, but this will
-    // work as an example
-    contents := Buf.String()
-    fmt.Println(contents)
-    // I reset the buffer in case I want to use it again
-    // reduces memory allocations in more intense projects
-    Buf.Reset()
-    // do something else
-    // etc write header
-    return
-}
+	
+	file, handler, err := r.FormFile("myfile")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.close()
+	
+	fmt.Fprintf(w, "%v", handler.Header)
+	f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+        defer f.Close()
+ 
+        io.Copy(f, file)
+ 
+    } 
+
+ 
+

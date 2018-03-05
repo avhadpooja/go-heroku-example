0ece475f8d8ec37324730c19c004d87f5efc7086
avhadpooja <36248454+avhadpooja@users.noreply.github.com>
2018-03-04 20:47:17 -0500

    Update web.go

--- web.go
+++ web.go
@@ -12,7 +12,7 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/upload", hello)
+	http.HandleFunc("/", hello)
 	fmt.Println("listening...")
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
@@ -20,35 +20,13 @@ func main() {
 	}
 }
 
-/*func hello(res http.ResponseWriter, req *http.Request) {
+func hello(res http.ResponseWriter, req *http.Request) {
 	t, _ := template.ParseFiles("client.html")
         t.Execute(res, nil)
-	//fmt.Fprintln(res, "hello, heroku")
-}*/
+	fmt.Fprintln(res, "hello, heroku")
+}
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	
-	t, _ := template.ParseFiles("client.html")
-        t.Execute(r, nil)
-	fmt.Fprintln(r, "hello, heroku")
-	file, handler, err := r.FormFile("uploadfile")
-        if err != nil {
-            fmt.Println(err)
-            return
-        }
-        defer file.Close()
- 
-        fmt.Fprintf(w, "%v", handler.Header)
-        f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
-        if err != nil {
-            fmt.Println(err)
-            return
-        }
-        defer f.Close()
- 
-        io.Copy(f, file)
- 
-    } 
+
 
  
 

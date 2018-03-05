c64455e635fc299c07b61d60fe44d6f78c73ad20
avhadpooja <36248454+avhadpooja@users.noreply.github.com>
2018-03-04 20:59:17 -0500

    Update web.go

--- web.go
+++ web.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/upload", hello)
 	fmt.Println("listening...")
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
@@ -18,13 +18,35 @@ func main() {
 	}
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
+/*func hello(res http.ResponseWriter, req *http.Request) {
 	t, _ := template.ParseFiles("client.html")
         t.Execute(res, nil)
 	fmt.Fprintln(res, "hello, heroku")
 }
+*/
 
-
+func hello(w http.ResponseWriter, r *http.Request) {
+ 
+   
+        file, handler, err := r.FormFile("uploadfile")
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+        defer file.Close()
+ 
+        fmt.Fprintf(w, "%v", handler.Header)
+        f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+        defer f.Close()
+ 
+        io.Copy(f, file)
+	fmt.Fprintf(f)
+ 
+}
 
  
 
